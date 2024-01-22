c73c1771d2f02f17435df8acebd5e0d2314b7931
Yassine Cherni <125700497+yassine-cherni@users.noreply.github.com>
2024-01-22 22:50:21 +0100

    Create Package.cpp

---
+++ Package.cpp
@@ -0,0 +1,32 @@
+#include "rclcpp/rclcpp.hpp"
+
+class Talker : public rclcpp::Node
+{
+public:
+    Talker()
+    : Node("talker")
+    {
+        publisher_ = this->create_publisher<std_msgs::msg::String>("topic", 10);
+        timer_ = this->create_wall_timer(
+            500ms, std::bind(&Talker::timer_callback, this));
+    }
+
+private:
+    void timer_callback()
+    {
+        auto message = std_msgs::msg::String();
+        message.data = "Hello, World!";
+        RCLCPP_INFO(this->get_logger(), "Publishing: '%s'", message.data.c_str());
+        publisher_->publish(message);
+    }
+    rclcpp::Publisher<std_msgs::msg::String>::SharedPtr publisher_;
+    rclcpp::TimerBase::SharedPtr timer_;
+};
+
+int main(int argc, char **argv)
+{
+    rclcpp::init(argc, argv);
+    rclcpp::spin(std::make_shared<Talker>());
+    rclcpp::shutdown();
+    return 0;
+}
