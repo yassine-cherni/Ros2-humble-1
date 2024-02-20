b1d4039231d21847f25755ae9741e3e2bbdf14a8
Yassine Cherni <125700497+yassine-cherni@users.noreply.github.com>
2024-02-20 18:20:53 +0100

    Update Mapping.cpp

--- Launch/Mapping.cpp
+++ Launch/Mapping.cpp
@@ -2,7 +2,7 @@
 #include "sensor_msgs/msg/laser_scan.hpp"
 #include "nav_msgs/msg/occupancy_grid.hpp"
 
-class MappingNode : public rclcpp::Node {
+clas MappingNode : public rclcpp::Node {
 public:
   MappingNode() : Node("mapping_node") {
     subscription_ = create_subscription<sensor_msgs::msg::LaserScan>(
