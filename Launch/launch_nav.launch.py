7caa72609f63e4aac17bc1ada25b76c01180aea0
Yassine Cherni <125700497+yassine-cherni@users.noreply.github.com>
2024-03-06 23:48:15 +0100

    Update launch_nav.launch.py

--- Launch/launch_nav.launch.py
+++ Launch/launch_nav.launch.py
@@ -8,7 +8,7 @@ from launch import LaunchDescription
 from launch.actions import IncludeLaunchDescription
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 
-from launch_ros.actions import Node
+from launch_ros.actions importode
 
 
 def generate_launch_description():
